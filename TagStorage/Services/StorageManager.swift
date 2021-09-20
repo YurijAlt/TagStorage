6029feaa5e1aeb97ff73bcb5e9f6a7cbe42cf881
Yurij Alt <yura_chekalyuk@mail.ru>
2021-09-20 15:56:49 +0300

    1. Added a transition to the screen with the addition of a tag

---
+++ TagStorage/Services/StorageManager.swift
@@ -0,0 +1,65 @@
+//
+//  StorageManager.swift
+//  TagStorage
+//
+//  Created by Юрий Чекалюк on 20.09.2021.
+//
+
+//import CoreData
+//
+//class StorageManager {
+//
+//    static let shared = StorageManager()
+//
+//    // MARK: Core Data stack
+//    private let persistentContainer: NSPersistentContainer = {
+//        let container = NSPersistentContainer(name: "Favorite_Cat")
+//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        })
+//        return container
+//    }()
+//
+//    private let viewContext: NSManagedObjectContext
+//    private init() {
+//        viewContext = persistentContainer.viewContext
+//    }
+//
+//
+//    func fetchData(completion: (Result<[FavoriteCat], Error>) -> Void) {
+//        let fetchRequest: NSFetchRequest<FavoriteCat> = FavoriteCat.fetchRequest()
+//        do {
+//            let favoriteCats = try viewContext.fetch(fetchRequest)
+//            completion(.success(favoriteCats))
+//        } catch let error {
+//            completion(.failure(error))
+//        }
+//    }
+//
+//    //MARK: Save and delete Data
+//    func save(_ favoriteCatURL: String) {
+//        let favoriteCat = FavoriteCat(context: viewContext)
+//        favoriteCat.catImage = favoriteCatURL
+//        saveContext()
+//    }
+//
+//    func delete(_ cat: FavoriteCat) {
+//        viewContext.delete(cat)
+//        saveContext()
+//    }
+//
+//    // MARK: CoreData saving support
+//    func saveContext() {
+//        if viewContext.hasChanges {
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
+//    }
+//
+//}
--- TagStorage/ViewControllers/AddNewTagViewController.swift
+++ TagStorage/ViewControllers/AddNewTagViewController.swift
@@ -12,8 +12,16 @@ class AddNewTagViewController: UIViewController {
     //MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func closeButtonTapped() {
+        dismiss(animated: true)
+    }
+    
 }
 
