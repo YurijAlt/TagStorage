1789560f4e8d04bec12d5dc765e14aaed14a761a
Yurij Alt <yura_chekalyuk@mail.ru>
2021-10-02 00:42:58 +0300

    1. Minor design changes

---
+++ TagStorage/ViewControllers/AdviceDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AdviceDetailViewController.swift
+//  TagStorage
+//
+//  Created by Юрий Чекалюк on 02.10.2021.
+//
+
+import UIKit
+
+class AdviceDetailViewController: UIViewController {
+    
+    //MARK: - IB Actions
+    @IBAction func closeButtonTapped() {
+        dismiss(animated: true)
+    }
+    
+}
